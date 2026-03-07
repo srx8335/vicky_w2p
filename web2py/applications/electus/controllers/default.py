d5dca842ba437ce222f7ac3523b7f1c2292efc56
srx8335 <srx8332@gmail.com>
2026-03-07 18:51:24 +0000

    Mar 7, 2026, 6:51 PM

--- web2py/applications/electus/controllers/default.py
+++ web2py/applications/electus/controllers/default.py
@@ -6,8 +6,7 @@
 
 # ---- example index page ----
 def index():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    dashboard()
 
 # ---- API (example) -----
 @auth.requires_login()
@@ -59,4 +58,8 @@ def download():
 
 
 
-# test
+
+
+
+def dashboard():
+    return dict()
