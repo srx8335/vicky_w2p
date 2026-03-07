d75b57b43b0948ad1b1523b071a6b4971feb8b35
srx8335 <srx8332@gmail.com>
2026-03-07 22:11:01 +0000

    Mar 7, 2026, 10:11 PM

--- web2py/applications/electus/models/menu.py
+++ web2py/applications/electus/models/menu.py
@@ -7,14 +7,14 @@
 
 response.menu = [
     (T('Home'), False, URL('default', 'home'), [])
-    (T('Home'), False, URL('default', 'home'), [])
-    (T('Home'), False, URL('default', 'home'), [])
-    (T('Home'), False, URL('default', 'home'), [])
-    (T('Home'), False, URL('default', 'home'), [])
-    (T('Home'), False, URL('default', 'home'), [])
-    (T('Home'), False, URL('default', 'home'), [])
+    (T('Finances'), False, URL('default', 'finances'), [])
+    (T('Pipeline'), False, URL('default', 'pipeline'), [])
+    (T('Clients'), False, URL('default', 'clients'), [])
+    (T('Tasks'), False, URL('default', 'tasks'), [])
+    (T('Reports'), False, URL('default', 'reports'), [])
+    (T('Scripts'), False, URL('default', 'scripts'), [])
 ]
 
 # ----------------------------------------------------------------------------------------------------------------------
 # provide shortcuts for development. you can remove everything below in production
-# ----------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------«~~«
