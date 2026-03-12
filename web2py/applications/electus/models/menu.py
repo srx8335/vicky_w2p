6d37c17646eb1ac065c5bd87d160e7228e343837
Ricardo Rodrigues <srx8332@gmail.com>
2026-03-12 15:13:02 +0000

    Mar 12, 2026, 3:13 PM

--- web2py/applications/electus/models/menu.py
+++ web2py/applications/electus/models/menu.py
@@ -9,22 +9,22 @@ response.menu = [
     (XML('<li class="nav-header"></li>'), False, None, []),
 
     (XML('<span class="material-icons">home</span><span> Home</span>'), request.function=='home', URL('default','home'), []),
-    (XML('<span class="material-icons">payments</span><span> Finances</span>'), False, URL('default','finances'), []),
-    (XML('<span class="material-icons">timeline</span><span> Pipeline</span>'), False, URL('default','pipeline'), []),
+    (XML('<span class="material-icons">payments</span><span> Finances</span>'), request.function=='finances', URL('default','finances'), []),
+    (XML('<span class="material-icons">timeline</span><span> Pipeline</span>'), request.function=='pipeline', URL('default','pipeline'), []),
     (XML('<span class="material-icons">groups</span><span> Clients</span>'), request.function=='clients', URL('default','clients'), []),
-    (XML('<span class="material-icons">task</span><span> Tasks</span>'), False, URL('default','tasks'), []),
-    (XML('<span class="material-icons">bar_chart</span><span> Reports</span>'), False, URL('default','reports'), []),
-    (XML('<span class="material-icons">code</span><span> Scripts</span>'), False, URL('default','scripts'), []),
+    (XML('<span class="material-icons">task</span><span> Tasks</span>'), request.function=='task', URL('default','tasks'), []),
+    (XML('<span class="material-icons">bar_chart</span><span> Reports</span>'), request.function=='reports', URL('default','reports'), []),
+    (XML('<span class="material-icons">code</span><span> Scripts</span>'), request.function=='scripts', URL('default','scripts'), []),
 
     (XML('<li class="nav-header">  </li>'), False, None, []),
 
-    (XML('<span class="material-icons">person</span><span> Profile</span>'), False, URL('default','profile'), []),
-    (XML('<span class="material-icons">settings</span><span> Settings</span>'), False, URL('default','settings'), []),
-    (XML('<span class="material-icons">logout</span><span> Logout</span>'), False, URL('default','logout'), []),
+    (XML('<span class="material-icons">person</span><span> Profile</span>'), request.function=='profile', URL('default','profile'), []),
+    (XML('<span class="material-icons">settings</span><span> Settings</span>'), request.function=='settings', URL('default','settings'), []),
+    (XML('<span class="material-icons">logout</span><span> Logout</span>'), request.function=='logout', URL('default','logout'), []),
 
     (XML('<li class="nav-header sidebar-bottom">  </li>'), False, None, []),
 
-    (XML('<span class="material-icons">info</span><span> About Us</span>'), False, URL('default','about'), []),
+    (XML('<span class="material-icons">info</span><span> About Us</span>'), request.function=='about', URL('default','about'), []),
 ]
 # ----------------------------------------------------------------------------------------------------------------------
 # provide shortcuts for development. you can remove everything below in production
