4b33e99faf60ca8f0a86654ebe0b29d5aabf90c4
Ricardo Rodrigues <srx8332@gmail.com>
2026-03-12 14:27:01 +0000

    Mar 12, 2026, 2:27 PM

--- web2py/applications/electus/models/menu.py
+++ web2py/applications/electus/models/menu.py
@@ -6,8 +6,6 @@
 # ----------------------------------------------------------------------------------------------------------------------
 response.menu = [
 
-    (XML('<li class="nav-header"></li>'), False, None, []),
-
     (XML('<span class="material-icons">home</span><span> Home</span>'), False, URL('default','home'), []),
     (XML('<span class="material-icons">payments</span><span> Finances</span>'), False, URL('default','finances'), []),
     (XML('<span class="material-icons">timeline</span><span> Pipeline</span>'), False, URL('default','pipeline'), []),
