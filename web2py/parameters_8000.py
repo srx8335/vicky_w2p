a742d57084c0c2d455af07568740d5e183144d9c
Ricardo Rodrigues <srx8332@gmail.com>
2026-03-13 14:21:36 +0000

    merge conflic resolvido, deixa de ser burro Ricardo🥰

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$bc85e21e21e45e71$9fe77f8d6dd6cbfb39137b2016ced56e50a83e47"
-=======
-password="pbkdf2(1000,20,sha512)$afc1a29bf40aa996$06c5b76ac0e0f5b96afdb0a15adf72af7a881b7c"
->>>>>>> 8759df1683391ec819a296d4fd6f9b8071f90ee5
+password="pbkdf2(1000,20,sha512)$ad3814effb179fca$60af349b55546e8dc4bd2ba38c688e927db1833c"
